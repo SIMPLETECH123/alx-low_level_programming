5c600438dc1e340bc994d91df7153eac3dae8274
Timilehin Caleb <simpletimi100@gmail.com>
2023-02-16 11:55:02 -0800

    c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#!/bin/bash
+include <stdio.h>
+
+/* C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line*/
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,"\n);
+	return 0;
+}
