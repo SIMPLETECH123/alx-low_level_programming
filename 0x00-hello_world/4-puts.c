8c93fa644f5b501c1601fd63f72eeaef10f36bf6
Timilehin Caleb <simpletimi100@gmail.com>
2023-02-16 15:03:35 -0800

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main -a program that print a line with puts function
+ * return 0 (success)
+ */
+int main()
+{
+	puts(""Programming is like building a multilingual puzzle\n");
+	return 0;
+}	
