c749bc9d4a6295273f396b7f7d3c8c2b3fb859f2
Timilehin Caleb <simpletimi100@gmail.com>
2023-02-17 10:05:38 -0800

    c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio>
+/**
+ * main - a program that print a line to the standard error
+ *
+ * Return: 1 (success)
+ */
+int main(void)
+{
+	write("and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19"\n);
+	return (1);
+}
