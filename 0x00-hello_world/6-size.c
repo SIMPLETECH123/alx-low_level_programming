2417e2463fc6c58f738e644b622c24167c338d35
Timilehin Caleb <simpletimi100@gmail.com>
2023-02-16 17:49:03 -0800

    c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	printf("size of the integer data type is %d\n", sizeof(int));
 	printf("size of character data type is %d\n", sizeof(char));
-	printf("size of float data type is %d\n", sizeof(float))
+	printf("size of float data type is %d\n", sizeof(float));
 	printf("size long long data type is %d\n", sizeof(long long int));
 	printf("size of a long integer data type is %d\n", sizeof(long int));
 	return (0);
