63c8bfb0e05dc484a659da4b736afa9615d6e19e
Timilehin Caleb <simpletimi100@gmail.com>
2023-02-16 17:45:09 -0800

    c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the size of various type
+ *
+ * Return: (0) success
+ */
+int main(void)
+{
+	printf("size of the integer data type is %d\n", sizeof(int));
+	printf("size of character data type is %d\n", sizeof(char));
+	printf("size of float data type is %d\n", sizeof(float))
+	printf("size long long data type is %d\n", sizeof(long long int));
+	printf("size of a long integer data type is %d\n", sizeof(long int));
+	return (0);
+}
