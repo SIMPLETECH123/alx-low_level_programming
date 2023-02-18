b88547ef449e68d614b3ceb5f3fa84b87ebe051e
Timilehin Caleb <simpletimi100@gmail.com>
2023-02-17 23:22:52 -0800

    a-z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - the alphabelt in lower case
+ *
+ * Return: 0 always (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+	putchar("%c/n", c);
+	return (0);
+i}
