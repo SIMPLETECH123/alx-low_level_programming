1b14dcfb5c035d6a47f319ce4427d009dd3c9cfd
Timilehin Caleb <simpletimi100@gmail.com>
2023-02-18 00:06:45 -0800

    if_alse_while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - alphabelt in lower case and in upper case
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'Z') {
+			putchar(d);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
+
