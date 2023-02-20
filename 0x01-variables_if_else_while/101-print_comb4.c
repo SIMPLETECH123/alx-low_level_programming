a97befb231fd97e1dd851bfa25028c7fdde06098
Timilehin Caleb <simpletimi100@gmail.com>
2023-02-20 03:20:03 -0800

    c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - all possible combinatio of 3 digits
+ *
+ * Return: 0 always (success)
+ */
+int main(void)
+{
+	int a = 0;
+
+	int b;
+
+	int c;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = a + 1; b < 10; b++)
+		{
+			for (c = b + 1; c < 10; c++)
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(c + '0');
+				if (!(a == 7 && b == 8 && c == 9))
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
