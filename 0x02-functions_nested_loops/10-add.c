5695da50c5a6d98a61205e86c08ce1fb8ddc77fa
Timilehin Caleb <simpletimi100@gmail.com>
2023-02-21 19:08:24 -0800

    loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add -add two integers and return the result
+ *
+ * @a: number one
+ * @b: number two
+ *
+ * Return: add of both numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_to_98 - all natural numbers fron n to 98
+ *
+ * @n: input number
+ *
+ * Return: no return
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else if (n < 98)
+	{
+		printf("%d, ", n);
+	}
+	printf("%d, ", n);
+}
