979942a9b7c01d88b14a6256552f0f802873a52f
Timilehin Caleb <simpletimi100@gmail.com>
2023-02-23 08:53:46 -0800

    c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 10 times numbers from 0 to 14
+ *
+ * Return: no return
+ */
+void more_numbers(void);
+{
+	int c;
+
+	for (c = 1; c <= 14; c++)
+	{
+		for (i = '0'; i <= '14'; i++)
+		{
+			if (i >= 10)
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
