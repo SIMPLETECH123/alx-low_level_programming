9d1dcecf40dc36d049d259950d010407d852554b
Timilehin Caleb <simpletimi100@gmail.com>
2023-02-23 05:44:26 -0800

    loops2

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * int _isdigit - a function that check for a digit
+ * @c - input character
+ * Return: 1 if it is a digit and 0 if it is'nt
+ */
+
+int _isdigit(int c);
+{
+	for ( c = 0; c <= 9, c++)
+	{
+		return (1);
+	}
+}
