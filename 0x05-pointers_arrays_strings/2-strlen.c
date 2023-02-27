dde565916c0e834321f13906ebfb4eba7a884b4a
Timilehin Caleb <simpletimi100@gmail.com>
2023-02-27 04:49:28 -0800

    pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the lenght of a string
+ * @str: the string to get the lenght of
+ * Return: the lenght of @str
+ */
+size_t _strlen(char *str)
+{
+	size_t lenght = 0;
+
+	while (*str++)
+	lenght++;
+	return (lenght);
+}
