684ee84ee4d6327f19b39e0b499688897da14f1e
Timilehin Caleb <simpletimi100@gmail.com>
2023-03-02 06:42:39 -0800

    c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - function that change lower to upper case
+ * @n: pointer
+ * Return: upper case char
+ */
+char *string_toupper(char *n)
+{
+	int i;
+	
+	for (i = 0; i < n; i++)
+	{
+		if (n[i] != '\0')
+		{
+			n[i] = n[i] - 32;
+		}
+	}
+	return (n);
+}
