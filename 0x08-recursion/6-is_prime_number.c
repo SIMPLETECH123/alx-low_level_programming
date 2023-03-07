1897d1b52e897a5ee46db2c3dae39aca0ac9ebc6
Timilehin Caleb <simpletimi100@gmail.com>
2023-03-07 04:53:54 -0800

    c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,14 +7,11 @@
  */
 int is_prime_number(int n)
 {
-	if (n == 1 || n < 0)
-		return (0);
-	if ((n % 1 == 0) && (n % n == 0))
-		return (1);
-		{
-			if (n == 1 || n < 0)
-				return (0);
-		}
-	else
+	while (n != 1)
+	{
+		if ((n % 1 == 0) && (n % n == 0))
+			return (1);
+	}
+	if (n == 1 || n <= 0)
 		return (0);
 }
