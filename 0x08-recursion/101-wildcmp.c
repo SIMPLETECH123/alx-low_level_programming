939348b226ec9f92b0d3dbb2bd2dc5f9424744e1
Timilehin Caleb <simpletimi100@gmail.com>
2023-03-08 03:42:06 -0800

    c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -18,7 +18,7 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	if (*s1 == '\0')
 	{
-		if (*s2 != '\0')
+		if (*s2 != '*')
 			return (0);
 		else
 			return (wildcmp(s1, s2 + 1));
