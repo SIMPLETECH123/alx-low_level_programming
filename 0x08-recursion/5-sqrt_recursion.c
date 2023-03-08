e404d979e765c45a3b23b506024c2bd13aaa93ba
Timilehin Caleb <simpletimi100@gmail.com>
2023-03-08 07:08:13 -0800

    c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,9 +8,8 @@
 int _sqrt_recursion(int n)
 {
 	if ((n / 2 * n / 2 == n) && n / 2 > 0)
-	{	return (n / 2);
-		(n / 2)--;
-	}
+		return (n / 2);
+	else
+		return (-1);
 	return ((_sqrt_recursion(n / 2) - 1));
-	return (-1);
 }
