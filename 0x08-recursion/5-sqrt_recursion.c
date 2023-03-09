fa3a518effb5fa509a1e5ae97ae06ef515f963ac
Timilehin Caleb <simpletimi100@gmail.com>
2023-03-09 06:11:22 -0800

    c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,18 +1,27 @@
 #include "main.h"
 
 /**
+ * subtractor - function that subtract consecutive odd numers from n
+ * @n: the number
+ * @i: prime number
+ * Return: times 0dd number were subtracted
+ */
+int subtractor(int n, int i)
+{
+	n -= i;
+	if (n == 0)
+		return ((i + 1) / 2);
+	if (n < 0)
+		return (-1);
+	return (subtractor(n, i + 2));
+}
+
+/**
  * _sqrt_recursion - square root.
  * @n: input
- * Return: 0
+ * Return:0 always
  */
 int _sqrt_recursion(int n)
 {
-	int i = n / 2;
-
-	if (i * i == n && i > 0)
-		return (i);
-	else
-		return (-1);
-	i--;
-	return (_sqrt_recursion(n));
+	return (n == 0 ? 0 : (subtractor(n, 1)));
 }
