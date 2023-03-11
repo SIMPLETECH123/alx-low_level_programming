2631afb4318834db1cbb6d2d56ccdcdcc3c1e7ad
Timilehin Caleb <simpletimi100@gmail.com>
2023-03-11 03:20:54 -0800

    c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - a pprogram that prints its name
+ * @argc:number of arhuement
+ * @argv: array of argument
+ *
  * Return: 0 always
  */
-int main(argc, *argv)
+int main(int argc, char *argv[])
 {
-	printf(argv[0]);
+	printf("%s/n", argv);
 	return (0);
 }
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int main(int argc_attribute_((unused)), char *argv[]);
+int _putchar(char c);
+#endif
