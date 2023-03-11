eb12613b54e38833af596b875f45d52da57c5f6d
Timilehin Caleb <simpletimi100@gmail.com>
2023-03-11 03:40:28 -0800

    c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main -print argc
+ * @argc: number of arguement
+ * @argv: array of argumeent
+ *
+ * Return: 0 always
+ */
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
