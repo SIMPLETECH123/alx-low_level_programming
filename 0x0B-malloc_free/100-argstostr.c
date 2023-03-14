5c6e3f4d795c4c770b42826d000f04b02958e30c
Timilehin Caleb <simpletimi100@gmail.com>
2023-03-14 05:46:01 -0700

    c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - function that concatenate all arguements
+ * @ac:arg1
+ * @av:areg2
+ * Return:arg to string
+ */
+char *argstostr(int ac, char **av)
+{
+	char *p;
+	int i, j, k, l, m;
+	int size = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			size++;
+		size++;
+	}
+	size++;
+	p = malloc(sizeof(char) * size);
+	if (p == NULL)
+		return (NULL);
+	m = 0;
+	for (k = 0; k < ac; k++)
+	{
+		for (l = 0; av[k][l] != '\0'; l++)
+		{
+			p[m] = av[k][l];
+			m++;
+		}
+	}
+	p[m] = '\0';
+	return (p);
+}
