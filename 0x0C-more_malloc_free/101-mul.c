ee0246e4a9da87dfea05ad6c34235c9c90f23689
Timilehin Caleb <simpletimi100@gmail.com>
2023-03-17 14:02:12 -0700

    c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
  * digitcheck - function that check for digits
@@ -53,16 +54,46 @@ void errors(void)
  * @argc: number of arguement
  * Return: 0 always success
  */
-int main( int argc, char *argv)
+int main(int argc, char *argv[])
 {
-	int i, d;
-	int mul, s1, s2;
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
-	if (argc != 2 || !digitcheck(s1) || !digitcheck(s2))
+	if (argc != 3 || !digitcheck(s1) || !digitcheck(s2))
 		errors();
-
-	mul = argv[1] * argv[2];
-	printf("%d\n", mul);
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
+	{
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
+	}
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
 	return (0);
 }
