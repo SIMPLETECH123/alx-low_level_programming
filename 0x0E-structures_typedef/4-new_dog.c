23e2344ea99bebab04ff60dec581ed8274f54524
Timilehin Caleb <simpletimi100@gmail.com>
2023-03-20 17:00:38 -0700

    c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * new_dog -  a function that creates a new dog.
+ * @name: name of the new dog
+ * @age:age of the new dog
+ * @owner: owner of the dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (new_dog == NULL)
+		return (NULL);
+}
