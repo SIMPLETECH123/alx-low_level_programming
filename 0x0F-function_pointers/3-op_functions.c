c2be382b7903d3d350290eefda51e586a54f916f
Timilehin Caleb <simpletimi100@gmail.com>
2023-03-23 12:58:48 -0700

    c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,67 @@
+#include "3-calc.h"
+
+/**
+ * op_add -function that adds
+ * @a:first integer
+ * @b:second integer
+ *
+ * Return:sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - function that subtract
+ * @a:first integer
+ * @b:second integer
+ *
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies
+ * @a:first integer
+ * @b:second integer
+ *
+ * Return: mul
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - division
+ * @a:first integer
+ * @b:second integer
+ *
+ * Return: int
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		return (-1);
+		exit(100);
+	}
+	return (a / b);
+}
+/**
+ * op_mod - modulus
+ * @a:first integer
+ * @b:second integer
+ *
+ * Return: int
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		return (-1);
+		exit(100);
+	}
+	return (a % b);
+}
