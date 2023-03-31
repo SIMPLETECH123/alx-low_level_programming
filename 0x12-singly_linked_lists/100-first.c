42f43c55f7dfe98b94399b8f1bf95512226a00a7
Timilehin Caleb <simpletimi100@gmail.com>
2023-03-31 06:40:33 -0700

    c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /**
- * _print - function that prints
- * @str: string
- * Return:string
+ * b - prints from init by the loader before main
  */
-void _print(void)
+void b(void) __attribute__ ((constructor));
+void b(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
