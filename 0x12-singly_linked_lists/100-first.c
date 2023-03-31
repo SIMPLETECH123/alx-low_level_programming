763daedf3dbb2cc0888f4c2c5eb3f02816df7143
Timilehin Caleb <simpletimi100@gmail.com>
2023-03-31 05:56:50 -0700

    c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * _print - function that prints
+ * @str: string
+ * Return:string
+ */
+void _print(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
