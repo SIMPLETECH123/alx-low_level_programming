bbb7771aa234bc248cea406f9a324e94d35798bc
Timilehin Caleb <simpletimi100@gmail.com>
2023-04-06 16:54:12 -0700

    c

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * reverse_listint -unction that reverses a listint_t linked list.
+ * @head:double struct pointer
+ * Return: a pointer to the first node of the reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	head++;
+	return (0);
+}
