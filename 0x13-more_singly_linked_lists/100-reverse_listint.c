9fef0bf660c8c58925b5184022683d33561e492f
Timilehin Caleb <simpletimi100@gmail.com>
2023-04-06 17:18:26 -0700

    c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -7,6 +7,17 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	head++;
-	return (0);
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	*head = prev;
+	return (prev);
 }
