7760e1896f0233228599266e8f547ce9ab075377
Timilehin Caleb <simpletimi100@gmail.com>
2023-04-09 15:24:22 -0700

    c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,10 @@
 #ifndef FILE
 #define FIlE
-#include <stddef.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdlib.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
