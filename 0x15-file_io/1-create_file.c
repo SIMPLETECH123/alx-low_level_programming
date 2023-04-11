6887a7e3738cc102efa99ced075685a842f1f1f9
Timilehin Caleb <simpletimi100@gmail.com>
2023-04-11 16:55:07 -0700

    c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (fd == -1)
+	if (fd < 0)
 		return (-1);
 	len = 0;
 	if (text_content != NULL)
@@ -21,15 +21,20 @@ int create_file(const char *filename, char *text_content)
 		if (text_content[len] != '\0')
 			len++;
 		res_write = write(fd, text_content, len);
-		if (res_write == -1)
+		if (res_write < 0)
 		{
 			write(1, "fails", 6);
 			return (-1);
 		}
 	}
-	else
+	else if (text_content == NULL)
 	{
-		res_write = write(fd, text_content, 0);
+		res_write = write(fd, text_content, len);
+		if (res_write < 0)
+		{
+			write(1, "fails", 6);
+			return (-1);
+		}
 	}
 	close(fd);
 	return (1);
